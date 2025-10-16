5c979dc52aa9ee00b951f221828f11eb42d84f3d
sathish2002Developer <sathishkumar.r@refex.co.in>
2025-10-16 17:51:41 +0530

    new changes

--- server/services/email_service.js
+++ server/services/email_service.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 class EmailService {
   constructor() {
     // Create transporter using SMTP configuration
-    this.transporter = nodemailer.createTransporter({
+    this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST || 'smtp.gmail.com',
       port: process.env.SMTP_PORT || 587,
       secure: false, // true for 465, false for other ports
